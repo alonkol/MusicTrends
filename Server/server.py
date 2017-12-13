a01d087b592593f20e33714e1d5a86d9a3662bfb
alonkol <alonkollmann@gmail.com>
2017-12-13 12:20:33 +0200

    added git ignore

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-import datetime
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello dear DB students!"
+    return "Wabadabadubdub!"
 
 if __name__ == '__main__':
-    app.run(port=8888, host="0.0.0.0", debug=True)
+    app.run(port=8888, host="localhost", debug=True)
