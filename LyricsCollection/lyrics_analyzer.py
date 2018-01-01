cf5ea6201a38cb6a901d527e8f1559f2e91baae4
noammansur <noam@optibus.co>
2018-01-01 19:12:56 +0200

    lyrics analyzer improvment

--- LyricsCollection/lyrics_analyzer.py
+++ LyricsCollection/lyrics_analyzer.py
@@ -1,14 +1,17 @@
 import json
+import re
 
 
 def create_words_map(full_text):
     """
-    This method takes full text and creates a mapping between each word in text to its count
+    This method takes full text and creates a mapping between each word in text to its count,
+    it ignores all non text characters and transfers all words to lower case
     :param full_text:
     :return: dict <word, word_count>
     """
     counts = dict()
-    words = full_text.split()
+    full_text = " ".join(re.findall("[a-zA-Z]+", full_text))
+    words = full_text.lower().split()
 
     for word in words:
         if word not in counts:
@@ -16,14 +19,3 @@ def create_words_map(full_text):
         counts[word] += 1
 
     return counts
-
-
-def create_word_count_for_lyrics():
-    data = json.load(open('lyrics.json'))
-    word_counts = {}
-    for song_name, lyrics in data.iteritems():
-        word_counts[song_name] = create_words_map(lyrics)
-
-    with open('lyrics_count.json', 'w') as lyrics_count_file:
-        json.dump(word_counts, lyrics_count_file)
-
