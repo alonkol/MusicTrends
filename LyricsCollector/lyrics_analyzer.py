09728ffa39623a029ae752edc5ab33e9abb16aca
noammansur <noam@optibus.co>
2018-01-01 16:17:51 +0200

    analyze word count for lyrics

---
+++ LyricsCollector/lyrics_analyzer.py
@@ -0,0 +1,31 @@
+import json
+
+
+def create_words_map(full_text):
+    """
+    This method takes full text and creates a mapping between each word in text to its count
+    :param full_text:
+    :return: dict <word, word_count>
+    """
+    counts = dict()
+    words = full_text.split()
+
+    for word in words:
+        if word not in counts:
+            counts[word] = 0
+        counts[word] += 1
+
+    return counts
+
+
+def create_word_count_for_lyrics():
+    data = json.load(open('lyrics.json'))
+    word_counts = {}
+    for song_name, lyrics in data.iteritems():
+        word_counts[song_name] = create_words_map(lyrics)
+
+    with open('lyrics_count.json', 'w') as lyrics_count_file:
+        json.dump(word_counts, lyrics_count_file)
+
+
+create_word_count_for_lyrics()
