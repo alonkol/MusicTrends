39a3969547e94adb2db3998305bcd27a23112137
noammansur <noam@optibus.co>
2018-01-10 15:32:00 +0200

    create also lyrics json in test jsons

--- TestJsons/create_test_json.py
+++ TestJsons/create_test_json.py
@@ -2,15 +2,15 @@ import json
 
 # ORIGINAL PATHS
 from LastFmApiHandler.retreive_data_from_last_fm import ARTISTS_FILE, SONGS_FILE
+from LyricsCollection.lyrics_collector import LYRICS_FILE
 
-
-LYRICS_PATH = '../LyricsCollection/' + 'lyrics.json'
+LYRICS_PATH = '../LyricsCollection/' + LYRICS_FILE
 ARTISTS_PATH = '../LastFmApiHandler/' + ARTISTS_FILE
 SONGS_PATH = '../LastFmApiHandler/' + SONGS_FILE
 
 
 TEST_ARTISTS_PATH = 'test_artists.json'
-TEST_SONGS_PATH = 'test_songs_with_mbid.json'
+TEST_SONGS_PATH = 'test_songs.json'
 TEST_LYRICS_PATH = 'test_lyrics.json'
 
 NUMBER_OF_CATEGORIES = 10
@@ -28,6 +28,7 @@ def write_data_to_file(data, filename):
 
 artists_data = get_data_from_file(ARTISTS_PATH)
 songs_data = get_data_from_file(SONGS_PATH)
+lyrics_data = get_data_from_file(LYRICS_PATH)
 
 new_artists_data = {}
 for count, (category, artist_list) in enumerate(artists_data.iteritems()):
@@ -36,10 +37,19 @@ for count, (category, artist_list) in enumerate(artists_data.iteritems()):
     new_artists_data[category] = artist_list[:NUMBER_OF_ARTIST_PER_CATEGORY]
 
 new_songs_data = {}
+mbids = []
 for artist_list in new_artists_data.itervalues():
     for artist in artist_list:
         new_songs_data[artist] = songs_data[artist][:NUMBER_OF_SONGS_PER_ARTIST]
+        mbids.extend([song_data[1] for song_data in new_songs_data[artist]])
+
+new_lyrics_data = {}
+for mbid in mbids:
+    data = lyrics_data.get(mbid)
+    if data is None:
+        continue
+    new_lyrics_data[mbid] = data
 
 write_data_to_file(new_artists_data, TEST_ARTISTS_PATH)
 write_data_to_file(new_songs_data, TEST_SONGS_PATH)
-
+write_data_to_file(new_lyrics_data, TEST_LYRICS_PATH)
