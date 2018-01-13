77783c79d92e062de5b7960f4eff54108658d96c
noammansur <noam@optibus.co>
2018-01-13 22:02:42 +0200

    fix query to match new tables

--- Server/queries.py
+++ Server/queries.py
@@ -358,8 +358,7 @@ ARTISTS_FOR_CATEGORIES = "SELECT Artists.artistID, Artists.artistName FROM Artis
 
 FIND_VIDEO_ID_BY_SONG_ID = "SELECT videoID From Videos WHERE songID = %s"
 
-UPDATE_VIDEOS_DATA = "UPDATE Videos SET viewCount = %s, likeCount = %s, dislikeCount = %s, favoriteCount = %s, " \
-                     "commentCount = %s WHERE videoID = %s"
+UPDATE_VIDEOS_DATA = "UPDATE Videos SET viewCount = %s, likeCount = %s, dislikeCount = %s WHERE videoID = %s"
 
 FIND_FIVE_MATCHING_SONG_NAMES = "SELECT songName " \
                                 "FROM " \
