7180deb09ee434970cdd00178c54e241d8c72dc9
Yossi Elman <yossi.elman@gmail.com>
2018-01-15 20:26:04 +0200

    fix for running on tomcat

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, send_from_directory
 from flask import render_template
 
+# fix in order to work on the tomcat server
+import sys
+sys.path.insert(0, "../")
+
 from Server.utilities import *
+
 
 app = Flask(__name__, static_folder='frontend-build', static_url_path='')
 
